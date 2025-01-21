a9c04b40c848b4f7062d7f91d10747ddb7274dc4
Rob McBryde <robertmcbryde83@gmail.com>
2025-01-21 13:28:31 +0000

    WeSplit - Creating Main UI with ability to select total amount of the bill and picker to select number of people who will be splitting the bill

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,19 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermoine", "Ron"]
-    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 15
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
         NavigationStack {
             Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-            .navigationTitle("Select a student")
+            .navigationTitle("WeSplit")
         }
     }
 }
